2ae80ce494887cf6e58e4f9329b582115596b4c8
Olivia Seto <oliviaseto5@gmail.com>
2023-11-06 15:00:08 -0500

    attempt at incorporating FB group component (react-facebook) on events page; attempted to include FB SDK (installed facebook-js-sdk types)

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,11 +1,13 @@
 // googleCalendarApiKey: 'AIzaSyB9OGdxbDWzEKeyxFaWzWhOypnSsmB-uvo',
 // events: 'c_o0ntjuei84bjmlpfqbcsr62u3c@group.calendar.google.com',
+// facebookAppId: '859184232616360'
 
 'use client';
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import { FacebookProvider, Group } from 'react-facebook';
 import 'app/globals.css'
 
 function GoogleCalendar() {
@@ -21,7 +23,26 @@ function GoogleCalendar() {
     fixedWeekCount: false
   };
 
-  return <FullCalendar {...calendarProps} />;
+  function FacebookGroup() {
+    return (
+      <FacebookProvider appId="859184232616360">
+        <Group
+          href="https://www.facebook.com/groups/csaatuva"
+          width="300"
+          showSocialContext={true}
+          showMetaData={true}
+          skin="light"
+        />
+      </FacebookProvider>
+    );
+  }
+
+  return (
+    <div>
+      <FullCalendar {...calendarProps} />
+      <FacebookGroup />
+    </div>
+  );
 }
 
 export default GoogleCalendar;
