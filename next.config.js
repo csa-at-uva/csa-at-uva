d05dab8c16dc496236b634856c87be55a7b332ac
Olivia Seto <oliviaseto5@gmail.com>
2023-12-21 17:54:06 -0500

    removed gallery pictures

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
-// @generated: @expo/next-adapter@2.1.52
-// Learn more: https://docs.expo.io/guides/using-nextjs/
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const { withExpo } = require("@expo/next-adapter");
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withFonts = require("next-fonts");
-const withTM = require("next-transpile-modules")(["react-native-web"]);
-
-const nextConfig = {
-  images: {
-    disableStaticImages: true,
-  },
-};
-
-module.exports = withPlugins(
-  [withTM, withExpo, withImages, withFonts],
-  nextConfig
-);
+module.exports = nextConfig
