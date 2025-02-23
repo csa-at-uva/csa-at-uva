644889a1fd7c3fef3875f866e8ea205daf874a82
Christopher Dai <christopher.daid@gmail.com>
2025-02-23 15:21:22 -0500

    Revert back to separate banner heights  (#37)
    * revert uniform banner heights from  feedback
    
    ---------
    
    Signed-off-by: Christopher Dai <christopher.daid@gmail.com>
    Signed-off-by: uvachinesestudentassociation <164442495+uvachinesestudentassociation@users.noreply.github.com>
    Co-authored-by: uvachinesestudentassociation <164442495+uvachinesestudentassociation@users.noreply.github.com>

--- app/components/AlumniCard.tsx
+++ app/components/AlumniCard.tsx
@@ -1,6 +1,6 @@
 import "@fontsource/raleway";
-import React, { useState } from 'react';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 interface AlumniCardProps {
   image?: string;
@@ -8,10 +8,7 @@ interface AlumniCardProps {
   children?: React.ReactElement | React.ReactElement[];
 }
 
-const AlumniCard: React.FC<AlumniCardProps> = ({
-  image,
-  children
-}) => {
+const AlumniCard: React.FC<AlumniCardProps> = ({ image, children }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -23,28 +20,28 @@ const AlumniCard: React.FC<AlumniCardProps> = ({
   };
 
   const cardStyles: React.CSSProperties = {
-    position: 'relative',
-    transition: 'transform 0.3s ease-in-out',
+    position: "relative",
+    transition: "transform 0.3s ease-in-out",
   };
 
   const overlayStyles: React.CSSProperties = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    textAlign: 'center',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center",
     opacity: isHovered ? 1 : 0,
-    visibility: isHovered ? 'visible' : 'hidden',
-    transition: 'opacity 0.3s ease, visibility 0.3s ease',
-    color: isHovered ? 'black' : 'transparent',
-    backgroundColor: isHovered ? 'white' : 'transparent',
-    padding: '20px', // Increase the padding for a wider overlay
-    borderRadius: '5px',
-    width: '80%', // Set a wider width for the overlay
+    visibility: isHovered ? "visible" : "hidden",
+    transition: "opacity 0.3s ease, visibility 0.3s ease",
+    color: isHovered ? "black" : "transparent",
+    backgroundColor: isHovered ? "white" : "transparent",
+    padding: "20px", // Increase the padding for a wider overlay
+    borderRadius: "5px",
+    width: "80%", // Set a wider width for the overlay
     maxHeight: "300px",
-    overflowY: 'auto',
+    overflowY: "auto",
     // maxWidth: '400px', // Limit the maximum width for better readability
-    margin: '0 auto', // Center the overlay horizontally
+    margin: "0 auto", // Center the overlay horizontally
   };
 
   // Splitting text into an array based on line breaks ("\n") and mapping them to Typography components
@@ -62,13 +59,11 @@ const AlumniCard: React.FC<AlumniCardProps> = ({
     >
       <CardMedia
         component="img"
-        style={{ width: '100%', height: '350px' }}
+        style={{ width: "100%" }}
         image={image}
         alt="Card Image"
       />
-      <CardContent style={overlayStyles}>
-        {children}
-      </CardContent>
+      <CardContent style={overlayStyles}>{children}</CardContent>
     </Card>
   );
 };
